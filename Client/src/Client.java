0df8238fe8f1a83c3b25bfe501f90833fa36d832
Kirill_Artamonov <artamonov.ks@yandex.ru>
2022-03-25 22:33:22 +0300

    Added dependency on Phone class

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,35 +1,23 @@
+import Connection.Phone;
+
 import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
-        try (
+        try (Phone phone = new Phone("127.0.0.1", 8000)) {
 
-            Socket socket = new Socket("127.0.0.1", 8000);
-
-            BufferedWriter writer =
-                    new BufferedWriter(
-                            new OutputStreamWriter(
-                                    socket.getOutputStream()));
-
-            BufferedReader reader =
-                    new BufferedReader(
-                            new InputStreamReader(
-                                    socket.getInputStream()));
-        ) {
             System.out.println("[client]: connected to server");
 
 //            Scanner in = new Scanner(System.in);
 //            System.out.println("Input the city:");
             String request = "Moscow";
-            writer.write(request);
-            writer.newLine();
-            writer.flush();
+            phone.writeLine(request);
+            String response = phone.readLine();
 
-            String response = reader.readLine();
-            System.out.println("[client]: Request: " + request);
-            System.out.println("[client]: Response: " + response);
+//            System.out.println("[dbg]: Request: " + request);
+//            System.out.println("[dbg]: Response: " + response);
 
         } catch (IOException e) {
             e.printStackTrace();
--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,3 +1,5 @@
+import Connection.Phone;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -10,30 +12,14 @@ public class Server {
             System.out.println("[server]: started!");
 
             while (true) {
-                try (
-                Socket socket = server.accept(); // conncection
-    //            System.out.println("[server]: client connected!");
-
-                BufferedWriter writer = // writer
-                        new BufferedWriter(
-                                new OutputStreamWriter(
-                                        socket.getOutputStream()));
-
-                BufferedReader reader =
-                        new BufferedReader(
-                                new InputStreamReader(
-                                        socket.getInputStream()));
-
-                ) {
-                    String request = reader.readLine();
-                    System.out.println("REQUEST: " + request);
+                try (Phone phone = new Phone (server)) {
+                    String request = phone.readLine();
+//                    System.out.println("[dbg]: REQUEST: " + request);
                     String response = "Hello, new client from " + request + " with the temperature: " + (int)Math.random();
-                    System.out.println("RESPONSE: " + response);
-                    writer.write(response);
-                    writer.newLine();
-                    writer.flush(); // send data before closing writer
+//                    System.out.println("[dbg]: RESPONSE: " + response);
+                    phone.writeLine(response);
 
-                } catch (NullPointerException e) {
+            } catch (NullPointerException e) {
                     e.printStackTrace();
                 }
             }
